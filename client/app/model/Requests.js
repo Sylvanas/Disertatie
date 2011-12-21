fd101068df117d77ccbe3d0cfdc27b21a5d7f0cd
Frunza <samuel@celsussolutions.com>
2011-12-21 17:08:56 +0200

    prepared to move files

--- client/app/model/Requests.js
+++
@@ -1,8 +0,0 @@
-Ext.define('App.model.Requests', {
-    extend: 'Ext.data.Model',
-    fields: [
-             {name: 'id', type: 'string'},
-             {name: 'name',  type: 'string'}
-             ],
-    idProperty: "id",
-});
--- client/app/model/SelectFriend.js
+++
@@ -1,8 +0,0 @@
-Ext.define('App.model.SelectFriend', {
-    extend: 'Ext.data.Model',
-    fields: [
-             {name: 'id', type: 'string'},
-             {name: 'name',  type: 'string'}
-             ],
-    idProperty: "id",
-});
