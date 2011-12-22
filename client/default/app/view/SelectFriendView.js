3d1cb41bf7b194609f04e541ea2a1575509f43f4
Frunza <samuel@celsussolutions.com>
2011-12-22 15:26:06 +0200

    removed test code

--- client/default/app/view/SelectFriendView.js
+++ client/default/app/view/SelectFriendView.js
@@ -1,34 +1,3 @@
-Ext.define('Contact', {
-   extend: 'Ext.data.Model',
-   fields: ['firstName', 'lastName']
-});
-
-var store = Ext.create('Ext.data.Store', {
-   model: 'Contact',
-   sorters: 'lastName',
-
-   getGroupString: function(record) {
-       return record.get('lastName')[0];
-   },
-
-   data: [
-       {firstName: 'Tommy',   lastName: 'Maintz'},
-       {firstName: 'Rob',     lastName: 'Dougan'},
-       {firstName: 'Ed',      lastName: 'Spencer'},
-       {firstName: 'Jamie',   lastName: 'Avins'},
-       {firstName: 'Aaron',   lastName: 'Conran'},
-       {firstName: 'Dave',    lastName: 'Kaneda'},
-       {firstName: 'Jacky',   lastName: 'Nguyen'},
-       {firstName: 'Abraham', lastName: 'Elias'},
-       {firstName: 'Jay',     lastName: 'Robinson'},
-       {firstName: 'Nigel',   lastName: 'White'},
-       {firstName: 'Don',     lastName: 'Griffin'},
-       {firstName: 'Nico',    lastName: 'Ferrero'},
-       {firstName: 'Nicolas', lastName: 'Belmonte'},
-       {firstName: 'Jason',   lastName: 'Johnston'}
-   ]
-});
-
 Ext.define('App.view.SelectFriendView', {
     extend: 'Ext.Panel',
     title: "SelectFriend View",
