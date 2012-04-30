58e696f4d3af8fce703d4edad46cdfd976e7ebfc
Frunza <samuel@celsussolutions.com>
2012-04-30 11:23:52 +0300

    test

--- client/default/app/utils.js
+++ client/default/app/utils.js
@@ -162,11 +162,11 @@ Ext.define('Global', {
 	
 	startSendingGeoData: function(){
 		Ext.Msg.alert('started');
-		$fh.geo(function(res){
+		/*$fh.geo(function(res){
 			Ext.Msg.alert('1:lon='+res.lon+', lat='+res.lat+', alt='+res.alt+', at='+res.when);
-		  });
+		  });*/
 		setTimeout(function sendGeoData() {
-			if(Ext.getStore('LocalStore').getAt(0).get('accountID') != ''){
+			//if(Ext.getStore('LocalStore').getAt(0).get('accountID') != ''){
 				//Ext.Msg.alert(Ext.getStore('LocalStore').getAt(0).get('accountID')+ Ext.getStore('LocalStore').getAt(0).get('email')+'-send location to cloud');
 				//setTimeout(sendGeoData, 10000);
 				if(App.Global.releaseCode){
@@ -185,7 +185,7 @@ Ext.define('Global', {
 					  });
 					setTimeout(sendGeoData, 10000);
 				}
-			}
+			//}
 		}, 1);
 	},
 });
